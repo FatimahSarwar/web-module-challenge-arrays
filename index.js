eda35b379ed23bd5cf9fcfb5d27a2fdcb6ecadfc
Fatimah Sarwar <fatimahsarwar@Fatimahs-MBP.fios-router.home>
2021-11-03 13:52:35 -0400

    Finished Task 1.

--- index.js
+++ index.js
@@ -45,10 +45,12 @@ Use the copy function below to do the following:
   2. Return a copy of the received array  
 */
 
-function copy(/*your code here*/){
-  /*your code here*/
+function copy(array){
+  const newArray = [];
+  newArray.push(array);
+  return array;
 }    
-
+console.log(copy( originalFlavors));
 
 
 
